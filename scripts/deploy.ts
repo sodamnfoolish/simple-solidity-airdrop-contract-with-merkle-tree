90d3ce5e921509da75d440cf00f54f5c267cc9f7
sodamnfoolish <n.zhyikov@gmail.com>
2022-07-07 06:03:37 +0300

    Added deploy task

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,76 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
+import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
+import { MerkleTree } from "merkletreejs";
+import { Airdrop__factory, ERC20__factory } from "../typechain-types";
+
+const abi = new ethers.utils.AbiCoder();
+
+const hash = (who: string, amount: BigNumber): string =>
+  ethers.utils.keccak256(
+    abi.encode(["address", "uint256"], [who, amount.toHexString()])
+  );
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const signer = (await ethers.getSigners())[0];
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const erc20Address = "0xdD2FD4581271e230360230F9337D5c0430Bf44C0"; // ERC20 address for airdrop (example)
 
-  await greeter.deployed();
+  const listToAirdrop = [
+    // list of addresses and amount for airdrop (example)
+    {
+      who: "0xdD2FD4581271e230360230F9337D5c0430Bf44C0",
+      amount: BigNumber.from(228),
+    },
+  ];
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log(
+    listToAirdrop.map((x) => {
+      return {
+        who: x.who,
+        amount: x.amount.toString(),
+      };
+    })
+  );
+
+  let amountToTransfer = BigNumber.from(0);
+
+  listToAirdrop.forEach(
+    (x) => (amountToTransfer = amountToTransfer.add(x.amount))
+  );
+
+  console.log(`Sum of amount to claim: ${amountToTransfer.toString()}`);
+
+  const merkleTree = new MerkleTree( // YOU SHOULD SAVE MERKLE TREE FOR FRONTEND INTERACTIONS!
+    listToAirdrop.map((x) => hash(x.who, x.amount)),
+    ethers.utils.keccak256,
+    {
+      sortPairs: true,
+    }
+  );
+
+  console.log("Deploying Airdrop contract");
+
+  const airdrop = await new Airdrop__factory(signer).deploy(
+    erc20Address,
+    merkleTree.getHexRoot()
+  );
+
+  await airdrop.deployed();
+
+  console.log("Airdrop contract has been deployed");
+
+  console.log(
+    `Sending ${amountToTransfer.toString()} token(s) to Airdrop contract`
+  );
+
+  await ERC20__factory.connect(erc20Address, signer).transfer(
+    airdrop.address,
+    amountToTransfer
+  );
+
+  console.log("Tokens have been sended");
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
